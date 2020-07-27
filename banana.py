ffc93692cc9b2ced47564879c41139eaa21c9db5
bgoumalatsos <bgoumalatsos@bethel.jw.org>
2020-07-27 12:44:32 -0400

    Optimized function checking if numbers get stuck in loop

--- banana.py
+++ banana.py
@@ -1,26 +1,31 @@
-import math
-
 class guardNode:
     def __init__(self, bananas):
         self.bananas = bananas
         self.loop = []
-        self.discard = []
-
-def inLoop(a, b):
-    powerOfTwo = int((a + b)/math.gcd(a, b))
-    return bool(powerOfTwo & (powerOfTwo - 1))
-
-def disconnect(nodeList,node):
-    for n in node.discard:
-        n.discard.remove(node)
-    for n in node.loop:
-        n.loop.remove(node)
+        self.bored = []
+        
+def inLoop(a,b):
+    if(a == b):
+        return False
+    if(a > b):
+        c = a
+        a = b
+        b = c
+    powerOfTwo = int(b/a)
+    powerOfTwo += 1
+    return powerOfTwo & (powerOfTwo - 1) 
+    
+def disconnect(nodeList, node):
+    for item in node.bored:
+        item.bored.remove(node)
+    for item in node.loop:
+        item.loop.remove(node)
     nodeList.remove(node)
 
-def removePair(nodeList,node1,node2):
-    disconnect(nodeList,node1)
-    disconnect(nodeList,node2)
-
+def removePair(nodeList, node1, node2):
+    disconnect(nodeList, node1)
+    disconnect(nodeList, node2)
+    
 def solution(banana_list):
     guards = [guardNode(banana_list[a]) for a in range(len(banana_list))]
     workingGuards = 0
@@ -31,19 +36,19 @@ def solution(banana_list):
                 guards[a].loop.append(guards[b])
                 guards[b].loop.append(guards[a])
             else:
-                guards[a].discard.append(guards[b])
-                guards[b].discard.append(guards[a])
+                guards[a].bored.append(guards[b])
+                guards[b].bored.append(guards[a])
 
-    if(len(banana_list)%2==1):
+    if(len(banana_list)%2 == 1):
         single=guardNode(-1)
         for guard in guards:
-            single.discard.append(guard)
-            guard.discard.append(single)
+            single.bored.append(guard)
+            guard.bored.append(single)
         guards.append(single)
-        workingGuards-=1
+        workingGuards -= 1
 
     while len(guards) > 0:
-        guards.sort(key = lambda x: len(x.discard), reverse = True)
+        guards.sort(key = lambda x: len(x.bored), reverse = True)
         currentGuard = guards[0]
         good = False
         for pair in guards[1:]:
@@ -56,7 +61,5 @@ def solution(banana_list):
             removePair(guards, currentGuard, guards[1])
 
     return workingGuards
-
-#print(inLoop(1,7))
+    
 print(solution([1, 7, 3, 21, 13, 19]))
-# [1, 7, 3, 21, 13, 19]
