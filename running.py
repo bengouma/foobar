02bbb2914d7365e2e14b50534ffc0c269f8705ab
bgoumalatsos <bgoumalatsos@bethel.jw.org>
2020-07-29 17:45:51 -0400

    WIP: Improve algorithm for running.py

--- running.py
+++ running.py
@@ -20,38 +20,40 @@ def solution(time, time_limit):
     start = 0
 
     while closed == False:
-        startPos = time[start]
+        currentPos = time[start]
         
         if(time_limit < 0):
             closed = True
 
-        if(min(startPos) == 0):
-            startPosTemp = sorted(startPos)
-            delta = startPosTemp[1]
-            nextPos = time[startPos.index(startPosTemp[1])]
-            start = time.index(nextPos)
-        else:
-            delta = min(startPos)
-            nextPos = time[startPos.index(min(startPos))]
+        if(min(currentPos) == 0):
+            currentPosTemp = sorted(currentPos)
+            delta = currentPosTemp[1]
+            nextPos = time[currentPos.index(currentPosTemp[1])]
+            if(time.index(nextPos) - 1 in savedBunnies):
+                nextPos = time[locationOfItems(currentPos, time.index(nextPos))[1]]
             start = time.index(nextPos)
 
-        if(time.index(startPos) - 1 in savedBunnies):
-            pass
         else:
-            if(startPos != time[0] and startPos != time[len(time) - 1]):
-                savedBunnies.append(time.index(startPos) - 1)
+            delta = min(currentPos)
+            nextPos = time[currentPos.index(min(currentPos))]
+            if(time.index(nextPos) - 1 in savedBunnies):
+                nextPos = time[locationOfItems(currentPos, time.index(nextPos))[1]]
+            start = time.index(nextPos)
+            
+        if(currentPos != time[0] and currentPos != time[len(time) - 1]):
+            savedBunnies.append(time.index(currentPos) - 1)
 
         time_limit -= delta
-        print(startPos, start, time_limit)
+        
+        if(len(savedBunnies) == len(time) - 2):
+            break
 
     return savedBunnies
 
-# print(solution([[0, 2, 2, 2, -1], 
-#                             [9, 0, 2, 2, -1], 
-#                             [9, 3, 0, 2, -1], 
-#                             [9, 3, 2, 0, -1], 
-#                             [9, 3, 2, 2, 0]], 1))
+# print(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
 
-
-print(locationOfItems([0, 2, 2, 2, -1], -1))
-# [[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3
+print(solution([[0, 2, 2, 2, -1], 
+                            [9, 0, 2, 2, -1], 
+                            [9, 3, 0, 2, -1], 
+                            [9, 3, 2, 0, -1], 
+                            [9, 3, 2, 2, 0]], 1))
